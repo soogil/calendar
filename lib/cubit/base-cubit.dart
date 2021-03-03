74a909d2ce2b6f9973c1ebb060eb4a6fb903b090
kimsoogil <zmfflrql5@gmail.com>
2021-03-03 21:29:54 +0900

    [soogil] base cubit set => emit 추가

--- lib/cubit/base-cubit.dart
+++ lib/cubit/base-cubit.dart
@@ -4,5 +4,5 @@ class BaseCubit<T> extends Cubit<T> {
   BaseCubit(state) : super(state);
 
   T get value => this.state;
-  set value(T state) => this.state;
+  set value(T state) => this.emit(state);
 }
