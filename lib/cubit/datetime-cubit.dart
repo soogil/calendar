a70b4db1d81778eec5d461781c2c33fba047e04d
kimsoogil <zmfflrql5@gmail.com>
2021-03-03 21:34:50 +0900

    [soogil] DateTimeCubit 수정

--- lib/cubit/datetime-cubit.dart
+++ lib/cubit/datetime-cubit.dart
@@ -4,15 +4,5 @@ import 'package:calendar/cubit/base-cubit.dart';
 class DateTimeCubit extends BaseCubit<DateTime> {
   DateTimeCubit({DateTime dateTime}) : super(dateTime ??= DateTime.now());
 
-  int get year => value.year;
-
-  set year(int year) => value = DateTime(year, value.month, value.day);
-
-  int get month => value.month;
-
-  set month(int month) => value = DateTime(value.year, month, value.day);
-
-  int get day => value.day;
-
-  set day(int day) => value = DateTime(value.year, value.month, day);
+  set dateTime(DateTime value) => this.value = value;
 }
